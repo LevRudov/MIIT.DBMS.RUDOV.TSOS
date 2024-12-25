1750e71ce59d41df4d02b8109a63cb5a405fd05c
LevRudov <94760234+LevRudov@users.noreply.github.com>
2024-12-25 12:15:47 +0300

    Update main.cpp

--- Demo/main.cpp
+++ Demo/main.cpp
@@ -11,7 +11,7 @@
 		setlocale(LC_ALL, "Russian");
 		SetConsoleCP(CP_UTF8);
 		SetConsoleOutputCP(CP_UTF8);
-		wordsList::Dictionary list1{ "glaza", "ochi","branitcy", "rugatcy" };
+		wordsList::Dictionary list1{ "глаза", "очи","браниться", "ругаться" };
 		list1.PushBack("window", "okno");
 		std::cout << list1.ToString() << "\n";
 		list1.Insert("table", "stol");
